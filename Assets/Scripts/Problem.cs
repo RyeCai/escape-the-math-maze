382a8426e691887ff76e36101748f77e15c4f1a2
RyeCai <70669000+RyeCai@users.noreply.github.com>
2022-12-05 23:29:22 -0500

    Functionally for multiple math problems

---
+++ Assets/Scripts/Problem.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; 
+using System.Globalization;
+using TMPro;
+
+public class Problem : MonoBehaviour
+{
+
+    private GameObject fps_player_obj;
+    private Level level;
+    private AudioSource source;
+    private AudioClip win_sound;
+    public TMP_InputField input;
+    public Button submit;
+    private Canvas panel;
+    // private GameObject p;
+    private int answer;
+    private int solution;
+    public TMP_Text question;
+    private Canvas problems;
+    private bool solved;
+    // public GameObject panel;
+    
+    // Use this for initialization
+    private int operand1;
+    private int operand2;
+    private string arith_operator;
+    string[] arith_operator_types = { "+", "-", "*", "/" };
+    private bool touching;
+    void Start()
+    {
+        touching = false;
+        fps_player_obj = GameObject.FindGameObjectWithTag("PLAYER");
+        //GameObject level_obj = GameObject.FindGameObjectWithTag("Level");
+        //level = level_obj.GetComponent<Level>();
+        //GameObject canvas_obj = GameObject.FindGameObjectWithTag("ProblemCanvas");
+        //problems = canvas_obj.GetComponent<Canvas>();
+        solved = false;
+        panel = GameObject.FindObjectOfType<Canvas>(true);
+        //panel = GameObject.Instantiate(prob) as GameObject;
+
+        //panel.gameObject.SetActive(true);
+        Debug.Log(panel);
+        //panel.transform.SetParent(problems.transform,false);
+
+        //Generate random math problem
+        bool hardest_problem = gameObject.tag == "door";
+        operand1 = hardest_problem ? Random.Range(3, 20) : Random.Range(2, 9);
+        operand2 = hardest_problem ? Random.Range(3, 20) : Random.Range(2, 9);
+        arith_operator = arith_operator_types[Random.Range(0, 3)];
+
+        switch (arith_operator)
+        {
+            case "+":
+                solution = operand1 + operand2;
+                break;
+            case "-":
+                int tempOp1 = operand1;
+                int tempOp2 = operand2;
+                operand1 = Mathf.Max(tempOp1, tempOp2);
+                operand2 = Mathf.Min(tempOp1, tempOp2);
+                solution = operand1 - operand2;
+                break;
+            case "*":
+                solution = operand1 * operand2;
+                arith_operator = "×";
+                break;
+            case "/":
+                //First operand becomes multiplication of generated numbers
+                solution = operand1;
+                operand1 *= operand2;
+                arith_operator = "÷";
+                break;
+        }
+
+        //if (level == null)
+        //{
+        //    Debug.LogError("Internal error: could not find the Level object - did you remove its 'Level' tag?");
+        //    return;
+        //}
+        
+        panel.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PLAYER" && !solved)
+        {
+            //Debug.Log("oop");
+            touching = true;
+            question.text = operand1.ToString() + arith_operator + operand2.ToString();
+            panel.gameObject.SetActive(true);
+            input.ActivateInputField();
+        }
+    }
+
+    //Decreases number of remaining problems if solved correctly
+    private void SubmitAnswer(){
+        
+        if (answer == solution){
+            solved = true;
+            Debug.Log("Yay");
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        } else{
+            Debug.Log("No");
+        }
+        panel.gameObject.SetActive(false);
+        touching = false;
+    }
+
+    private void Update()
+    {
+        if (panel.gameObject.activeSelf && touching)
+        {
+            float distance = (gameObject.transform.position - fps_player_obj.gameObject.transform.position).magnitude;
+            bool correctForm = int.TryParse(input.text, out int a);
+            answer = correctForm ? a : answer;
+            submit.onClick.RemoveAllListeners();
+            submit.onClick.AddListener(SubmitAnswer);
+            if (distance > 2 || Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+            {
+                panel.gameObject.SetActive(false);
+                touching = false;  
+            } else if (Input.GetKeyUp(KeyCode.Return))
+            {
+                Debug.Log("Enter Pressed");
+                SubmitAnswer();
+            }
+        }
+    }
+
+}
