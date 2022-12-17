e2eacdba084b2a284a70848288b326d453625944
Jayden Nambu <jnambu@umass.edu>
2022-12-17 17:41:54 -0500

    PatrolAndSeek check angles and walls
    Patrol and Seek only detect player if they are in angle and not through walls

--- Assets/Scripts/PatrolAndSeek.cs
+++ Assets/Scripts/PatrolAndSeek.cs
@@ -10,11 +10,22 @@ public class PatrolAndSeek : MonoBehaviour
     private Vector3 target;
     private GameObject fps_player_obj;
     private float radius_of_search_for_player;
+    private Transform sight;
+    private Vector3 distToPlayer;
+    private Vector3 dirToPlayer;
+    private Vector3 forwardVector;
+    private float angle;
     // Start is called before the first frame update
     void Start()
     {
+        // foreach(Transform child in transform){
+        //     if(child.name == "Sight"){
+        //         sight = child;
+        //         break;
+        //     }
+        // }
         fps_player_obj = GameObject.FindGameObjectWithTag("PLAYER");
-        radius_of_search_for_player = 10.0f;
+        radius_of_search_for_player = 30.0f;
         index = 0;
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();    
         target = points[index].position;
@@ -24,11 +35,20 @@ public class PatrolAndSeek : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 distToPlayer = transform.position - fps_player_obj.transform.position;
+        distToPlayer = transform.position - fps_player_obj.transform.position;
         distToPlayer.y = 0;
-        if(distToPlayer.magnitude < radius_of_search_for_player){
+        dirToPlayer = fps_player_obj.transform.position - transform.position;
+        dirToPlayer.Normalize();
+        var dot = Vector3.Dot(dirToPlayer, transform.forward);
+        // sight.transform.LookAt(fps_player_obj.transform);
+        // Physics.Raycast(sight.transform.position, sight.transform.forward, 100)
+        Debug.Log(dot);
+        RaycastHit hit;
+        // if player is in sight
+        if(distToPlayer.magnitude < radius_of_search_for_player && dot > 0.707 && Physics.Raycast(transform.position, dirToPlayer, out hit) && hit.collider.tag == "PLAYER"){
             target = fps_player_obj.transform.position;
             agent.SetDestination(target);
+            Debug.Log("sighted");
         }else{
             Vector3 dist = transform.position - target;
             dist.y = 0;
@@ -36,7 +56,6 @@ public class PatrolAndSeek : MonoBehaviour
                 index = (index + 1) % points.Length;
                 target = points[index].position;
                 agent.SetDestination(target);
-
             }
         }      
     }
