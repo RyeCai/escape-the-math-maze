fa2a8110a79df2bb018c13c7d993d0146fb24b9e
Jayden Nambu <jnambu@umass.edu>
2022-12-20 18:58:42 -0500

    Updated Prefabs
    Updated colors, rand is red patrol is blue. Patrol now picks two points within 10 from their spawn and patrols those points

--- Assets/Scripts/PatrolAndSeek.cs
+++ Assets/Scripts/PatrolAndSeek.cs
@@ -6,7 +6,7 @@ public class PatrolAndSeek : MonoBehaviour
 {
     private Animator animation_controller;
     UnityEngine.AI.NavMeshAgent agent;
-    public Transform[] points;
+    private Vector3[] points;
     private int index;
     private Vector3 target;
     private GameObject fps_player_obj;
@@ -15,14 +15,26 @@ public class PatrolAndSeek : MonoBehaviour
     private Vector3 dirToPlayer;
     private Vector3 forwardVector;
     private float angle;
+    private float xMax;
+    private float xMin;
+    private float zMax;
+    private float zMin;
     // Start is called before the first frame update
     void Start()
     {
+        xMax = Mathf.Min(transform.position.x + 10, 64);
+        xMin = Mathf.Max(transform.position.x - 10, 0);
+        zMax = Mathf.Min(transform.position.z + 10, 64);
+        zMin = Mathf.Max(transform.position.z - 10, 0);
         fps_player_obj = GameObject.FindGameObjectWithTag("PLAYER");
         radius_of_search_for_player = 10.0f;
         index = 0;
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();    
-        target = points[index].position;
+        // target = points[index].position;
+        for(int i = 0; i < 2; i++){
+            points[i] = new Vector3(Random.Range(xMin, xMax), 0, Random.Range(zMin, zMax));
+        }
+        target = points[index];
         agent.SetDestination(target);
         animation_controller = GetComponent<Animator>();
     }
@@ -37,17 +49,17 @@ public class PatrolAndSeek : MonoBehaviour
         var dot = Vector3.Dot(dirToPlayer, transform.forward);
         RaycastHit hit;
         // if player is in sight
-        if(distToPlayer.magnitude < radius_of_search_for_player && dot > 0.707 && Physics.Raycast(transform.position, dirToPlayer, out hit) && hit.collider.tag == "PLAYER" && !StaticData.invisible){
+        if(distToPlayer.magnitude > 1 && distToPlayer.magnitude < radius_of_search_for_player && dot > 0.707 && Physics.Raycast(transform.position, dirToPlayer, out hit) && hit.collider.tag == "PLAYER" && !StaticData.invisible){
         // if(!StaticData.invisible){
-            target = fps_player_obj.transform.position;
-            agent.SetDestination(target);
+        agent.SetDestination(fps_player_obj.transform.position);
             // Debug.Log("sighted");
         }else{
+            agent.SetDestination(target);
             Vector3 dist = transform.position - target;
             dist.y = 0;
             if(dist.magnitude < 1){
                 index = (index + 1) % points.Length;
-                target = points[index].position;
+                target = points[index];
                 agent.SetDestination(target);
             }
         }      
